65a22bc4ed8a267f46f29fffc3eb40ffbc986368
MrPABITRAPRADHAN <gpabitrapadhan@gmail.com>
2025-03-16 23:50:26 +0530

    String basic

--- String/traverse_01.js
+++ String/traverse_01.js
@@ -1,1 +1,26 @@
 let str = "pabitra"
+
+
+//? length of str
+console.log(str.length)
+
+//? convert string to array
+// let a = str.split(",");
+// console.log(a)
+
+//? convert char Array to string
+// console.log(a.toString())
+
+
+//? reverse a string
+let s = str.split("").reverse().join("");
+console.log(s.toString())
+
+//? traverse string using for loop
+// for(let i = 0; i <str.length; i++) {
+//     console.log(`${i}th charachter is : ${str.charAt(i)}`)
+// }
+
+//? traverse string using for of loop
+// let str1 = "Hello";
+// str1.split("").map(char => console.log(char));
