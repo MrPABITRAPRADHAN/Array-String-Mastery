b60049f7aa51cfb61cbcc56230e7fb0da4b20f7b
MrPABITRAPRADHAN <gpabitrapadhan@gmail.com>
2025-03-16 23:13:28 +0530

    Array Palindrome

---
+++ Array/palindrome_03.js
@@ -0,0 +1,21 @@
+let arr = [1,2,4,3,2,1]
+let l = 0
+let h = arr.length-1;
+let temp =false
+
+for(let i=0;i<arr.length;i++){
+
+   while(l<h){
+     if(arr[l]==arr[h]){
+        l++;
+        h--;
+        temp =true;
+        break;
+     }else{
+        temp =false;
+        break;
+     }
+   }
+}
+
+console.log(temp==true?"palindrome":"not a palindrome")
